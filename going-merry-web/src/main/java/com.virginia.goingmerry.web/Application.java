f93fbe129641b299e4567a10ccf7b75f4b01ff06
Virginia <wujy@glodon.com>
2018-08-22 20:33:10 +0800

    build project

---
+++ going-merry-web/src/main/java/com.virginia.goingmerry.web/Application.java
@@ -0,0 +1,9 @@
+package com.virginia.goingmerry.web;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
