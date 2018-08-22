bbf5e696abd04161ff46ac3ee839a3ee12b8eeca
Virginia <wujy@glodon.com>
2018-08-22 21:00:10 +0800

    build project

--- going-merry-web/src/main/java/com.virginia.goingmerry.web/Application.java
+++ going-merry-web/src/main/java/com.virginia.goingmerry.web/Application.java
@@ -1,6 +1,10 @@
 package com.virginia.goingmerry.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
+
 public class Application {
 
     public static void main(String[] args) {
---
+++ going-merry-web/src/main/java/com.virginia.goingmerry.web/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.virginia.goingmerry.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author wujy , 2018/8/15
+ */
+@RestController
+public class DemoController {
+    @GetMapping("/go")
+    public String go() {
+        return "success";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "welcome to hello world";
+    }
+}
