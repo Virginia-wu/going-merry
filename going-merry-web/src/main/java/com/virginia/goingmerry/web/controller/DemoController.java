8633c434034235f3aa616719e5ce312b82320539
吴佳云 <wujy@glodon.com>
2018-12-21 18:40:28 +0800

    基础项目的设计

--- going-merry-web/src/main/java/com/virginia/goingmerry/web/controller/DemoController.java
+++ going-merry-web/src/main/java/com/virginia/goingmerry/web/controller/DemoController.java
@@ -1,20 +1,18 @@
-package com.virginia.going.merry.web.controller;
+package com.virginia.goingmerry.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author wujy , 2018/8/15
  */
 @RestController
+@RequestMapping
 public class DemoController {
-    @GetMapping("/go")
-    public String go() {
-        return "success";
-    }
 
     @GetMapping("/")
     public String home() {
-        return "welcome to hello world";
+        return "going-merry: hello world";
     }
 }
---
+++ going-merry-web/src/test/java/com/example/demo/MainTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.Date;
+
+/**
+ * @Author wujy , 2018/9/18
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
