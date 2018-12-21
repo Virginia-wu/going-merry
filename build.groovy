0265adbb398475822c6b8cbce26776ed66613dcf
吴佳云 <wujy@glodon.com>
2018-12-21 19:03:46 +0800

    基础项目的设计

---
+++ build.groovy
@@ -0,0 +1,38 @@
+node {
+    def dockerImage
+    def imageName = 'going-merry'
+    def serviceName = 'going-merry'
+    def registryUrl = 'registry.virginia.com:5000'
+    def httpRegistryUrl = 'http://'+registryUrl
+    def maven = docker.image('maven:3-alpine')
+
+    stage('checkout') {
+
+        checkout scm
+    }
+
+    stage('MvnBuild') {
+
+        maven.inside('-v /root/.m2/:/root/.m2/'){
+            sh 'mvn -T 4 -P docker clean install '
+        }
+    }
+
+    stage('BuildImage') {
+
+        dockerImage = docker.build(imageName)
+    }
+
+    stage('PushImage') {
+
+        docker.withRegistry(httpRegistryUrl) {
+
+            dockerImage.push("t_$BUILD_NUMBER")
+            dockerImage.push("latest")
+        }
+    }
+    stage('AppDeploy') {
+
+        sh  'docker service update --update-order start-first --image '+registryUrl+'/'+imageName+":t_$BUILD_NUMBER  stack_"+serviceName
+    }
+}
